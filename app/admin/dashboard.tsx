5fa87be19a63336e83a7307791f2298cfd72339c
Raphael9143 <s3jack3s@gmail.com>
2025-04-04 20:22:04 +0700

    [update] header component
    simply add header to tabs layout

--- app/admin/dashboard.tsx
+++ app/admin/dashboard.tsx
@@ -2,16 +2,9 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useRouter } from "expo-router";
 import { View, Button } from "react-native";
 
-export default function Dashboard() {
-    const router = useRouter()
-    const handleLogout = async (event: { preventDefault: () => void; }) => {
-        event.preventDefault()
-        await AsyncStorage.removeItem('user'); 
-        router.replace('/(auth)/login');
-    }
+export default function Dashboard() {    
     return (
         <View>
-            <Button title="Logout" onPress={handleLogout}/>
         </View>
     )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,18 @@
-import { View } from "react-native";
+import { ROUTES } from "@/constants/routes";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useRouter } from "expo-router";
+import { View, Button } from "react-native";
 
 export default function Header() {
+    const router = useRouter()
+    const handleLogout = async (event: { preventDefault: () => void; }) => {
+            event.preventDefault()
+            await AsyncStorage.removeItem('user'); 
+            router.replace(ROUTES.login);
+        }
     return (
         <View>
-            
+            <Button title="Log out" onPress={handleLogout}/>
         </View>
     )
 }
