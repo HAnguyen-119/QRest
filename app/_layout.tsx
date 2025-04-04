a2d93b34e7632ba1ad0529e9344850391acb09df
Raphael9143 <s3jack3s@gmail.com>
2025-04-04 22:49:25 +0700

    [update] useFonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,10 +2,18 @@ import { Stack } from 'expo-router';
 import { View, ActivityIndicator } from 'react-native';
 import { useAuth } from '@/hooks/useAuth';
 
+import { useFonts } from 'expo-font'
+
 export default function RootLayout() {
   const loading = useAuth()
 
-  if (loading) {
+  const [loaded] = useFonts({
+    'JosefinSans-Regular': require('../assets/fonts/Josefin_Sans/static/JosefinSans-Regular.ttf')
+  })
+
+  console.log(loaded)
+  //test: checkout /staff/dashboard
+  if (loading && !loaded) {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <ActivityIndicator size="large" />
--- app/staff/dashboard.tsx
+++ app/staff/dashboard.tsx
@@ -1,5 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+
 export default function Dashboard() {
     return (
-        <></>
+        <View>
+            <Text style={styles.text}>test font</Text>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    text: {
+        fontFamily: 'JosefinSans-Regular',
+        fontSize: 24,
+    }
+})
