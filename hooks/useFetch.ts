a6bf1c35ef678a545f9292f9f5dc3996bdf10e08
Raphael9143 <s3jack3s@gmail.com>
2025-04-10 15:58:12 +0700

    [fix] missing promise function

--- hooks/useFetch.ts
+++ hooks/useFetch.ts
@@ -4,28 +4,29 @@ import { useEffect, useState } from "react"
 
 export const useFetch = (type: GetData) => {
     const [data, setData] = useState<any>(null)
-
-    const fetchData = async () => {
-        try {
-            let response = null
-            switch(type) {
-                case 'orders':
-                    response = fetchAPI.getOrders()
-                    break
-                case 'foods': 
-                    response = fetchAPI.getFood()
-                    break
-                case 'categories':
-                    response = fetchAPI.getCategories()
-                    break
-                default:
-                    console.error(`Error, type not found, add '${type}' to constants/types.ts and try again`)
-            }
-            setData(response)
-        } catch (error) {
-            console.log('Error while fetching data: ', error)
-        } 
-
-    }
-    return { data, fetchData }
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                let response = null
+                switch(type) {
+                    case 'orders':
+                        response = await fetchAPI.getOrders()
+                        break
+                    case 'foods': 
+                        response = await fetchAPI.getFood()
+                        break
+                    case 'categories':
+                        response = await fetchAPI.getCategories()
+                        break
+                    default:
+                        console.error(`Error, type not found, add '${type}' to constants/types.ts and try again`)
+                }
+                setData(response)
+            } catch (error) {
+                console.log('Error while fetching data: ', error)
+            } 
+        }
+        fetchData()
+    }, [type])
+    return { data }
 }
