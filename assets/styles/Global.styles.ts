4ca4660e097fcb3b6f3a4a2b1213b9b0ab439d6c
Raphael9143 <s3jack3s@gmail.com>
2025-04-18 11:21:07 +0700

    [styles] revert global styles

---
+++ assets/styles/Global.styles.ts
@@ -0,0 +1,14 @@
+import { COLORS } from "@/constants/colors";
+import { StyleSheet } from "react-native";
+
+export const createGlobalStyles = (isDark: boolean) => {
+    return StyleSheet.create({
+        text: {
+            color: isDark ? COLORS.light : COLORS.dark,
+            fontFamily: 'Josefin-Sans'
+        },
+        background: {
+            backgroundColor: isDark ? COLORS.dark : COLORS.light
+        }
+    })
+}
