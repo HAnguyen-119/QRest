0ef186d1c33c0b0fea7743497a8a2acc5dc7a7ba
HAnguyen <anh.nh11920@gmail.com>
2025-05-29 22:20:01 +0700

    [Feat] Post food, staff with image

--- app/admin/_layout.tsx
+++ app/admin/_layout.tsx
@@ -2,10 +2,10 @@ import TabsLayout from "../(tabs)/_layout";
 
 export default function AdminLayout() {
     const routes = [
-        {name: 'dashboard', title: 'Dashboard', icon: 'home' as const},
+        {name: 'dashboard', title: 'Dashboard', icon: 'home-outline' as const},
         {name: 'menu', title: 'Menu', icon: 'fast-food-outline' as const},
         {name: 'table', title: 'Table', icon: 'add-circle-outline' as const},
-        {name: 'staff', title: 'Staff', icon: 'person-add-outline' as const},
+        {name: 'staff', title: 'Staff', icon: 'people-outline' as const},
     ]
     return <TabsLayout routes={routes}/>
 }
